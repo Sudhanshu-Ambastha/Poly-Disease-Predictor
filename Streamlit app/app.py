fc5b586111b4527754487442e12ef8c52ce94dde
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-01-11 09:00:50 +0530

    test

--- Streamlit app/app.py
+++ Streamlit app/app.py
@@ -50,8 +50,8 @@ with st.sidebar:
 # Load data
 if selected == "🩸 Diabetes Prediction" or selected == "❤️ Heart Disease Prediction":
     # Load data for diabetes and heart disease prediction
-    diabetes_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\diabetes.csv')  # Update with your diabetes dataset path
-    heart_disease_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\heart.csv')  # Update with your heart disease dataset path
+    diabetes_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\Streamlit app\\diabetes.csv')  # Update with your diabetes dataset path
+    heart_disease_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\Streamlit app\\heart.csv')  # Update with your heart disease dataset path
 
     # Training SVM model for diabetes prediction
     X_diabetes = diabetes_data.drop(columns='Outcome', axis=1)
@@ -78,7 +78,7 @@ if selected == "🦠 Multiple Disease Prediction":
     st.title("Multiple Disease Prediction using Symptoms")
 
     # Load data for multiple disease prediction
-    train_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\Training.csv')
+    train_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\Streamlit app\\Training.csv')
     test_data = pd.read_csv('C:\\Users\\sudha\\OneDrive\\Documents\\GitHub\\combined-disease-prediction-test\\Testing.csv')
 
     # Split data into features and target variable
